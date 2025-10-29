c2331b5c714568269d8df94aab08a148a1d2b83b
unknown <siragenius1@gmail.com>
2025-10-29 22:44:06 +0530

    merge

---
+++ MergeArrays.java
@@ -0,0 +1,35 @@
+public class MergeArrays {
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        int[] merged = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            merged[k++] = arr2[j++];
+        }
+
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5};
+        int[] b = {2, 4, 6};
+        int[] result = mergeSortedArrays(a, b);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
