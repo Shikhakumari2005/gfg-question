d337b962b1723dd1812e6dd33ff43662084b5b4e
unknown <siragenius1@gmail.com>
2025-11-19 23:34:51 +0530

    gfg

---
+++ fdg.java
@@ -0,0 +1,7 @@
+blic class fgh {
+    public static void main(String[] args){
+        int[] arr={1,4,4,6,6,6,6,8,8,8,9,3,4 ,6,6};
+        int res[]=remove(arr);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + "");
+            
